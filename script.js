e950e84b867a228afb5d095bfcdbf16c77bbf60f
cjsdiniz <cjsdiniz@outlook.com>
2022-03-23 17:34:11 +0000

    Modal working

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+function openModel() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+}
+
+function closeModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+}
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModel);
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
